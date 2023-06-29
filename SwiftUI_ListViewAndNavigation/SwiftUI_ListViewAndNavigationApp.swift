37102a6dd04b6879de76ea8d9059ceea9cee7fb2
bzamanbd <riponzamanbd@gmail.com>
2023-06-29 22:04:46 +0600

    Initial Commit

---
+++ SwiftUI_ListViewAndNavigation/SwiftUI_ListViewAndNavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ListViewAndNavigationApp.swift
+//  SwiftUI_ListViewAndNavigation
+//
+//  Created by Jesmin Nancy on 29/6/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ListViewAndNavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
